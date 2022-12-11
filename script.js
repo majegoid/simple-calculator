9ec721283dd935808008af8b553c8158c9550ebf
majegoid <majegoid@gmail.com>
2022-12-11 12:39:06 -0600

    adds element selector calls

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+// state
+let runningTotal = 0;
+let currentOperator = undefined;
+let displayValue = '';
+
+// get access to elements
+let keyClearDiv = document.querySelector('#key-clear');
+let keyDeleteDiv = document.querySelector('#key-delete');
+
+let key0Div = document.querySelector('#key-0');
+let key1Div = document.querySelector('#key-1');
+let key2Div = document.querySelector('#key-2');
+let key3Div = document.querySelector('#key-3');
+let key4Div = document.querySelector('#key-4');
+let key5Div = document.querySelector('#key-5');
+let key6Div = document.querySelector('#key-6');
+let key7Div = document.querySelector('#key-7');
+let key8Div = document.querySelector('#key-8');
+let key9Div = document.querySelector('#key-9');
+
+let keyPlusDiv = document.querySelector('#key-plus');
+let keySubtractDiv = document.querySelector('#key-subtract');
+let keyMultiplyDiv = document.querySelector('#key-multiply');
+let keyDivideDiv = document.querySelector('#key-divide');
+
+let keyPeriodDiv = document.querySelector('#key-period');
+let keyEqualsDiv = document.querySelector('#key-equals');
+
+let displayDiv = document.querySelector('#calculator-display');
+
+// add event listeners
+key7Div.onclick = (e) => {
+  displayValue += e.currentTarget.id.split('-')[1];
+};
+
 function add(a, b) {
   return a + b;
 }
@@ -18,4 +53,6 @@ function operate(operatorFunc, a, b) {
   operatorFunc(a, b);
 }
 
-// get access to elements
+function updateDisplay() {
+  
+}
